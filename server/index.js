67afa83e1fc5268b107c5f48cbd809f861f9a291
Joe Wright <joewright87@gmail.com>
2018-02-21 00:29:17 -0500

    init

---
+++ server/index.js
@@ -0,0 +1,81 @@
+/* global require, process, console, __dirname, exports */
+'use strict';
+const {Client} = require('pg');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+exports.run = run;
+run();
+function run() {
+	app.set('pgClient', getPGClient());
+
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use(logger());
+	app.get('/phrases/:id', function(req, res) {
+		var queryText = 'SELECT * FROM clips WHERE id = $1';
+		app.get('pgClient').query(queryText, [req.params.id], function(err, result) {
+			if (err || !result || !result.rows[0]) {
+				return res.status(404).send({error: 'not found'});
+			}
+			return res.send(result.rows[0]);
+		});
+	});
+
+	app.post('/save', bodyParser.json(), function(req, res) {
+		var insertTxt = 'INSERT INTO clips(content, voice) VALUES($1, $2) RETURNING *';
+		var values = [req.body.content, req.body.voice || ''];
+		app.get('pgClient').query(insertTxt, values, function(err, result) {
+			if (err) {
+				return res.status(422).send({errors: 'Failed to save, lol'});
+			}
+			res.send(result.rows[0]);
+		});
+	});
+
+	app.listen(process.env.PORT, function() {
+		console.log('Dang web server ready for it at port', process.env.PORT);
+	});
+
+	process.on('SIGTERM', function() {
+		console.log('exiting');
+		process.exit();
+	});
+}
+
+function getPGClient() {
+	const client = new Client(process.env.PGURI);
+	// console.log('dip');
+	client.connect(function(err) {
+		if (err) throw err;
+		console.log('postgresql: connected');
+		setupDB(client);
+	});
+	return client;
+}
+
+function setupDB(client) {
+	const createTableText = `
+	CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+	CREATE TEMP TABLE IF NOT EXISTS clips (
+	  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+	  content varchar,
+	  voice varchar,
+	  created timestamp DEFAULT CURRENT_TIMESTAMP
+	);
+	`;
+	// create our temp table
+	client.query(createTableText, function(err) {
+		if (err) throw err;
+		console.log('postgresql: clips table initialized');
+	});
+}
+
+function logger() {
+	return function(req, res, next) {
+		console.log(req.path);
+		next();
+	};
+}
---
+++ server/public/player.js
@@ -0,0 +1,149 @@
+/* global SpeechSynthesisUtterance, console */
+(function() {
+    'use strict';
+    var synth = window.speechSynthesis;
+
+    var inputForm = document.getElementById('form');
+    var inputTxt = document.getElementById('input');
+    var voiceSelect = document.getElementById('select');
+    var saveButton = document.getElementById('save');
+
+    var voices = synth.getVoices();
+
+    setupSynth();
+    setupSave();
+    checkUrlParams();
+
+    function checkUrlParams() {
+        // https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript#2880929
+        var urlParams;
+        var match,
+            pl = /\+/g, // Regex for replacing addition symbol with a space
+            search = /([^&=]+)=?([^&]*)/g,
+            decode = function(s) {
+                return decodeURIComponent(s.replace(pl, " "));
+            },
+            query = window.location.search.substring(1);
+
+        urlParams = {};
+        match = search.exec(query);
+        while (match) {
+            urlParams[decode(match[1])] = decode(match[2]);
+            match = search.exec(query);
+        }
+        if (urlParams.id) {
+            loadMyThing(urlParams.id);
+        }
+    }
+
+    function setupSynth() {
+        for (var i = 0; i < voices.length; i++) {
+            var option = document.createElement('option');
+            option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+            option.setAttribute('data-lang', voices[i].lang);
+            option.setAttribute('data-name', voices[i].name);
+            voiceSelect.appendChild(option);
+        }
+
+        inputForm.onsubmit = function(event) {
+            event.preventDefault();
+
+            var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+            utterThis.voice = getVoice();
+
+            synth.speak(utterThis);
+            inputTxt.blur();
+        };
+    }
+
+    function setupSave() {
+        var saving;
+        saveButton.addEventListener('click', function saveClick() {
+            if (saving || !inputTxt.value) {
+                return false;
+            }
+            saving = true;
+            var voice = getVoice();
+            httpRequest({
+                method: 'POST',
+                url: '/save',
+                body: {
+                    content: inputTxt.value,
+                    voice: voice && voice.voiceURI
+                }
+            }, function(err, result) {
+                if (err) {
+                    console.error(err);
+                }
+                saving = false;
+                document.location.search = '?id=' + result.id;
+            });
+        });
+    }
+
+    function loadMyThing(id) {
+        httpRequest({
+            method: 'GET',
+            url: '/phrases/' + id
+        }, function(err, result) {
+            if (err) {
+                inputTxt.value = 'lol, we can\'t find your stuff';
+                return console.error(err);
+            }
+            inputTxt.value = result.content;
+            if (voices && voices.length) {
+                var selectedName;
+                for (var i = 0; i < voices.length; i++) {
+                    if (voices[i].voiceURI === result.voice) {
+                        selectedName = voices[i].name;
+                    }
+                }
+                for (var x = 0; x < voiceSelect.childNodes.length; x++) {
+                    let option = voiceSelect.childNodes[x];
+                    option.selected = false;
+                    if (option && option.getAttribute('data-name') === selectedName) {
+                        option.selected = 'selected';
+                    }
+                }
+            }
+        });
+    }
+
+    function getVoice() {
+        var result;
+        if (voiceSelect.selectedOptions[0]) {
+            var name = voiceSelect.selectedOptions[0].getAttribute('data-name');
+            for (var i = 0; i < voices.length; i++) {
+                if (voices[i].name === name) {
+                    result = voices[i];
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    function httpRequest(options, callback) {
+
+        var req = new XMLHttpRequest();
+        req.addEventListener('load', function loaded() {
+            var data;
+            if (req.status !== 200) {
+                return callback(req);
+            }
+            try {
+                data = JSON.parse(req.responseText);
+            } catch (e) {
+                return callback(e);
+            }
+            callback(null, data);
+        });
+        req.open(options.method, options.url);
+        req.setRequestHeader('Content-Type', 'application/json');
+        if (options.body) {
+            req.send(JSON.stringify(options.body));
+        } else {
+            req.send();
+        }
+    }
+})();
